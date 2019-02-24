dd9627a434a19f9a8c4fbe1bbb41ee7783bb0e54
antoniochl <alu0101038377@ull.edu.es>
2019-02-24 04:12:01 +0000

    second it failed

--- databases/test/parse-rdf-test.js
+++ databases/test/parse-rdf-test.js
@@ -6,7 +6,12 @@ const rdf=fs.readFileSync(`${__dirname}/pg132.rdf`);
 const parseRDF=require('../lib/parse-rdf.js');
 
 describe('parseRDF',()=>{
-	it('should be a	function',() =>	{
+	it('should be a function',() =>	{
 		expect(parseRDF).to.be.a('function');	
+	});
+
+	it('should parse RDF content',() => {
+		const	book = parseRDF(rdf);
+		expect(book).to.be.an('object');
 	});
 });
