92a4cd43b56bb7c83a41d9afa3e44e80625d86f7
antoniochl <alu0101038377@ull.edu.es>
2019-02-24 04:31:45 +0000

    second it property title passed

--- databases/lib/parse-rdf.js
+++ databases/lib/parse-rdf.js
@@ -7,5 +7,6 @@ module.exports=rdf=>{
 	const $ = cheerio.load(rdf);
 	const book = {};
 	book.id	=+ $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/','');
+	book.title = $('dcterms\\:title').text();
 	return book;
 };
